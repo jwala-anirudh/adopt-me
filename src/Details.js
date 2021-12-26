440f959cff559fd6589853c59aab45be3c0b78b5
Anirudh Jwala <anirudhsai2@gmail.com>
2021-12-26 23:25:46 +0530

    Final release with pet adopt modal

--- src/Details.js
+++ src/Details.js
@@ -6,12 +6,16 @@ import ErrorBoundary from "./ErrorBoundary";
 
 import ThemeContext from "./ThemeContext";
 
+import Modal from "./Modal";
+
 class Details extends Component {
   constructor() {
     super(); // important & necessary
+
     // useState()
     this.state = {
       loading: true,
+      showModal: false,
     };
   }
 
@@ -35,11 +39,15 @@ class Details extends Component {
     );
   }
 
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
+  adoptThePet = () => window.open("http://bit.ly/pet-adopt", "_blank");
+
   render() {
     if (this.state.loading) {
       return <h2>Loading...</h2>;
     }
-    const { animal, breed, city, state, description, name, images } =
+    const { animal, breed, city, state, description, name, images, showModal } =
       this.state;
     return (
       <div className="details">
@@ -49,10 +57,26 @@ class Details extends Component {
           <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
           <ThemeContext.Consumer>
             {([theme]) => (
-              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
+                Adopt {name}
+              </button>
             )}
           </ThemeContext.Consumer>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <button onClick={this.adoptThePet}>Yes</button>
+                  <button onClick={this.toggleModal}>Not today</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
