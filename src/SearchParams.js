ac4e772fff03d24fdc10c3ee96f109c17e47079d
Anirudh Jwala <anirudhsai2@gmail.com>
2021-12-26 14:50:32 +0530

    Page with results

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 // custom hook for searching breeds
 import useBreedList from "./useBreedList";
-import Pet from "./Pet";
+
+import Results from "./Results";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -26,7 +27,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -70,14 +76,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          key={pet.id}
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
