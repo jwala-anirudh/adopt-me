d8b65eda44532ec2a623f0f38cfd303255226223
Anirudh Jwala <anirudhsai2@gmail.com>
2021-12-26 22:30:38 +0530

    Error handling for API failures

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import { withRouter } from "react-router-dom";
 
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
   constructor() {
@@ -52,4 +53,12 @@ class Details extends Component {
   }
 }
 
-export default withRouter(Details);
+const DetailsWithRouter = withRouter(Details);
+
+export default function DetailsWithErrorBoundary() {
+  return (
+    <ErrorBoundary>
+      <DetailsWithRouter />
+    </ErrorBoundary>
+  );
+}
